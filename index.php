5da5883fac6b308b251b55aae7516efff6119c1b
gerlikesk <168183244+gerlikesk@users.noreply.github.com>
2024-04-26 14:28:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1>Meie pojekti pealeht</h1>
     <p>See leht arvestab ka muudatustega</p>
-    <img src="https://static.wikia.nocookie.net/assassinationclassroom/images/c/c5/Koro_sensei_transparent.png/revision/latest?cb=20161202043210">
+    <img src="https://static.wikia.nocookie.net/assassinationclassroom/images/c/c5/Koro_sensei_transparent.png">
   </body>
 </html>
