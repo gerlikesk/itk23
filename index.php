f3350d3b1feaeaad595735be35858c9234f1b94e
gerlikesk <168183244+gerlikesk@users.noreply.github.com>
2024-05-19 23:48:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <p>See leht arvestab ka muudatustega</p>
     <img src="https://www.vecteezy.com/photo/25284015-close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai">
     <img src="https://images.unsplash.com/photo-1618886487325-f665032b6352?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-    <img src="https://static.wikia.nocookie.net/assassinationclassroom/images/c/c5/Koro_sensei_transparent.png">
+    <img src="https://images.unsplash.com/photo-1617635277889-df22350f81a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
   </body>
 </html>
